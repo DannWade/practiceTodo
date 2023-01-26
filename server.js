5b2df6dcb3810c3512952beaa52573628d20f60d
cheekygerold <spanishoceango@gmail.com>
2023-01-25 20:54:03 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr,{useUnifiedTopology:true})
+    .then(client=>{
+        console.log(`Connected to ${dbName} database`)
+        db = client.db(dbName)
+    })    
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/', async (req,res)=>{
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed:false}) 
+    res.render('index.ejs',{items:todoItems,left:itemsLeft})
+})
+
+app.post('/addTodo',(req,res)=>{
+    db.collection('todos').insertOne({thing:req.body.todoItem, completed:false})
+    .then(result=>{
+        console.log('Todo Added')
+        res.redirect('/')
+    })
+    .catch(error=>console.error(error))
+})
+
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
